fa2154ff489aa91592c5c1e7e148fada6c4f4204
adellaaye <adellasuryaananda01@gmail.com>
2018-09-03 13:42:05 +0700

    first commit

---
+++ seeds/user.js
@@ -0,0 +1,7 @@
+const users = require('../Users')
+exports.seed = function(knex, Promise) {
+  return knex('users').del()
+    .then(function () {
+      return knex('users').insert(users);
+    });
+};
